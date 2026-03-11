db7fa351e0bd4f053b489962959b78cba04515d0
thefirey33 <mmethefirey@gmail.com>
2026-03-11 19:53:19 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Thefirey33's Fun Stuff.
+
+A website where I upload whatever game, activity I can make in web development.
+
+## Requirements
+
+- npm
+
+...and that's pretty much it.
+
+
+## Running & Building for Production
+
+To run this website, use:
+
+```shell
+npm run dev
+```
+
+To build this website for production, use:
+
+```shell
+npm run dev
+```
+
+Then, after it's built, test it with:
+
+```shell
+npm run preview
+```
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import './layout.css';
+	import Logo from '$lib/assets/img/firey_83c.png';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={Logo} />
+	<title>thefirey33's fun stuff.</title>
+</svelte:head>
+
+<div
+	class="flex min-h-20 w-screen justify-center border-b border-gray-500 p-4 tracking-widest select-none md:text-3xl"
+>
+	<div class="flex flex-col items-center">
+		<h1>thefirey33's fun stuff.</h1>
+		<h1 class="text-xl">for funsies.</h1>
+	</div>
+</div>
+
+<div class="mr-auto ml-auto max-w-300 p-5">
+	{@render children()}
+</div>
